3a77cc484fcbff873eab308bbd5fd9434a7ae880
hilal-najem <hilal123work@gmail.com>
2023-02-21 15:29:39 +0200

    feat: Add toggle between dark and light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 
 import { TranslateService } from '@ngx-translate/core';
+
+import { AppPage } from '@models/index';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   public languages: string[] = ['en', 'ar'];
   public language: string = this.languages[0];
 
-  public appPages = [];
+  public appPages: AppPage[] = [];
 
   constructor(
     public platform: Platform,
@@ -23,6 +25,31 @@ export class AppComponent {
       this.translate.addLangs(this.languages);
       this.translate.setDefaultLang(this.language);
       this.translate.use(this.language);
-  });
+    });
+  }
+
+  ngOnInit(): void {
+    const color: AppPage = {
+      type: 'toggle',
+      title: 'menu.theme'
+    };
+    this.appPages.push(color);
+  }
+
+  toggleChanged(event: any, p: AppPage): void {
+    switch(p.title) {
+      case 'menu.theme': {
+        this.toggleTheme(event);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  toggleTheme(event: any): void {
+    event.detail.checked ? 
+    document.body.setAttribute('color-theme', 'dark') : document.body.setAttribute('color-theme', 'light');
   }
 }
---
+++ src/app/models/app-page.model.ts
@@ -0,0 +1,5 @@
+export type AppPage = {
+    icon?: string;
+    type: string;
+    title: string;
+}
