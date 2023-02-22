50dbbe7c3862e6fb1293f3a66af53317a6f76b6d
hilal-najem <hilal123work@gmail.com>
2023-02-22 18:22:39 +0200

    feat: Add basic tests and functions for login page

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,26 +1,42 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 
 import { LoginPage } from './login.page';
 
 import { imports, providers } from '@shared/imports';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from '@app/app-routing.module';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
-      imports: imports,
-      providers: providers
+      imports: [
+        ...imports,
+        AppRoutingModule
+      ],
+      providers: [
+        ...providers,
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to home page on login', fakeAsync(() => {
+    spyOn(router, 'navigate');
+    component.login();
+    tick(1500);
+    expect(router.navigate).toHaveBeenCalledWith(['/home']);
+  }));
 });
