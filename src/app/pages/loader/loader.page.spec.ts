f78aa00b7ff531a3565a80ee4f838e23cb5ce388
hilal-najem <hilal123work@gmail.com>
2023-02-22 18:15:23 +0200

    feat: add tests for loader

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,24 +1,36 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from '@app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoaderPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), AppRoutingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.get(Router);
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to login page after load', fakeAsync(() => {
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+    tick(1500);
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  }));
 });
